7fcfb2ecc9cb9baf11e1354307c85f1c08bf8147
elena-dragunova <33746898+elena-dragunova@users.noreply.github.com>
2019-01-10 16:37:28 +0500

    first commit

---
+++ github.js
@@ -0,0 +1,23 @@
+class GitHub {
+    constructor() {
+        this.client_id = "476e4cb364f42fc2733d";
+        this.client_secret = "ce39044fc7e25fe8acecd0ee841b3f0f3a85c13c";
+        this.repos_count = 5;
+        this.repos_sort = "created: asc";
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        const repos = await reposResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
